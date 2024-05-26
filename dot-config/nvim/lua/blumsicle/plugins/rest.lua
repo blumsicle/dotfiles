675d5c27d1b297b0c81c04dba15c170097f5cb85
blumsicle <blumsicle@icloud.com>
2024-05-25 22:17:28 -0600

    Add keymaps for rest.nvim

--- dot-config/nvim/lua/blumsicle/plugins/rest.lua
+++ dot-config/nvim/lua/blumsicle/plugins/rest.lua
@@ -13,7 +13,25 @@ return {
 		ft = "http",
 		dependencies = { "luarocks.nvim" },
 		config = function()
-			require("rest-nvim").setup()
+			require("rest-nvim").setup({})
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("RestKeys", {}),
+				pattern = { "http" },
+				callback = function()
+					local set = vim.keymap.set
+					set("n", "<leader>hh", ":Rest run<cr>", { desc = "Run HTTP request", buffer = true })
+					set("n", "<leader>hn", ":Rest result next<cr>", { desc = "Goto next result tab", buffer = true })
+					set(
+						"n",
+						"<leader>hp",
+						":Rest result prev<cr>",
+						{ desc = "Goto previous result tab", buffer = true }
+					)
+					set("n", "<leader>hL", ":Rest logs<cr>", { desc = "View HTTP request logs", buffer = true })
+					set("n", "<leader>hl", ":Rest last<cr>", { desc = "Run last HTTP request", buffer = true })
+				end,
+			})
 		end,
 	},
 }
