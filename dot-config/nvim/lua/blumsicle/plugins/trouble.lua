fd1d6c280c98794acbe7e68f841798eca5c0683a
blumsicle <blumsicle@icloud.com>
2024-06-01 00:11:27 -0600

    Update trouble

--- dot-config/nvim/lua/blumsicle/plugins/trouble.lua
+++ dot-config/nvim/lua/blumsicle/plugins/trouble.lua
@@ -1,12 +1,29 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	cmd = "Trouble",
+	opts = {},
 	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Open/close trouble list" },
-		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Open trouble document diagnostics" },
-		{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Open trouble quickfix list" },
-		{ "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Open trouble location list" },
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
 		{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Open todos in trouble" },
+		{
+			"<leader>xl",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xq",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
 }
