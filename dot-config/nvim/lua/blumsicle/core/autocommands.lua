07728c8d45b22ab2b636767739f31f4297fabf1b
blumsicle <blumsicle@icloud.com>
2024-12-15 19:59:50 -0700

    Fix deprecation warning

--- dot-config/nvim/lua/blumsicle/core/autocommands.lua
+++ dot-config/nvim/lua/blumsicle/core/autocommands.lua
@@ -6,6 +6,6 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" }, {
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
 	group = vim.api.nvim_create_augroup("YankHighlight", {}),
 	callback = function()
-		vim.highlight.on_yank()
+		vim.hl.on_yank()
 	end,
 })
