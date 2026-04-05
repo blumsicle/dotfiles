05d3fd68f095351f45f8859406217590c7d1af65
blumsicle <me@blumsicle.dev>
2026-04-04 22:22:32 -0600

    Add keymap

--- dot-config/nvim/lua/blum/plugins/mini_workflow.lua
+++ dot-config/nvim/lua/blum/plugins/mini_workflow.lua
@@ -16,6 +16,29 @@ return {
         local mini_visits = require("mini.visits")
         local mini_trailspace = require("mini.trailspace")
 
+        local files_with_hidden = function()
+            local tool = vim.fn.executable("rg") == 1 and "rg"
+                or vim.fn.executable("fd") == 1 and "fd"
+                or vim.fn.executable("git") == 1 and "git"
+                or "fallback"
+
+            if tool == "fallback" then
+                return mini_pick.builtin.files()
+            end
+
+            local commands = {
+                rg = { "rg", "--files", "--hidden", "--glob", "!.git", "--color=never" },
+                fd = { "fd", "--type=f", "--hidden", "--exclude", ".git", "--color=never" },
+                git = { "git", "-c", "core.quotepath=false", "ls-files", "--cached", "--others", "--exclude-standard", "--directory" },
+            }
+
+            return mini_pick.builtin.cli({
+                command = commands[tool],
+            }, {
+                source = { name = string.format("Files (%s, hidden)", tool) },
+            })
+        end
+
         mini_pick.setup()
         mini_extra.setup()
         require("mini.bracketed").setup()
@@ -25,8 +48,10 @@ return {
         mini_visits.setup()
 
         u.kset("n", "<leader>ff", mini_pick.builtin.files, { desc = "Find files in cwd" })
+        u.kset("n", "<leader>fF", files_with_hidden, { desc = "Find files in cwd, including hidden" })
         u.kset("n", "<leader>fb", mini_pick.builtin.buffers, { desc = "Find open buffers" })
         u.kset("n", "<leader>fg", mini_pick.builtin.grep_live, { desc = "Find string in cwd" })
+        u.kset("n", "<leader>fG", mini_extra.pickers.git_files, { desc = "Find git files" })
         u.kset("n", "<leader>fh", mini_pick.builtin.help, { desc = "Find help" })
         u.kset("n", "<leader>fk", mini_extra.pickers.keymaps, { desc = "Find keymaps" })
         u.kset("n", "<leader>fv", mini_extra.pickers.visit_paths, { desc = "Find visited paths" })
